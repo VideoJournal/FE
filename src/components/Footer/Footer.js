1809f59600be0bd6b938b8422188119da3f45325
Sorin Chis <kish.sorin@yahoo.com>
2020-01-16 17:49:10 +0200

    added footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,56 @@
 import React from "react";
+import { Row, Col } from "antd";
+import styled from "styled-components";
 
-const Footer = () => {
+export default function Footer() {
   return (
-    <div>
-      <h>Best looking footer in the world</h>
-    </div>
+    <Root>
+      <footer className="footer page-wrapper">
+        <div className="footer-wrap page"></div>
+        <div className="footer-bottom">
+          <div className="page">
+            <Row>
+              <Col
+                md={4}
+                xs={24}
+                style={{ textAlign: "left" }}
+                className="mobile-hide"
+              ></Col>
+              <Col md={20} xs={24}>
+                <span
+                  className="mobile-hide"
+                  style={{
+                    lineHeight: "16px",
+                    paddingRight: 12,
+                    marginRight: 11
+                  }}
+                >
+                  <a href="empty" rel="noopener noreferrer" target="_blank">
+                    Video Journal
+                  </a>
+                </span>
+                <span style={{ marginRight: 24 }} className="mobile-hide">
+                  <a href="empty" rel="noopener noreferrer" target="_blank">
+                    Lambda School
+                  </a>
+                </span>
+                <span style={{ marginRight: 12 }}>Logo maybe</span>
+                <span style={{ marginRight: 12 }}>
+                  Project Made During Lambda X
+                </span>
+              </Col>
+            </Row>
+          </div>
+        </div>
+      </footer>
+    </Root>
   );
-};
+}
 
-export default Footer;
+const Root = styled.div`
+  .footer {
+    padding: 30px 0;
+    background: linear-gradient(to bottom, #031d6a, #000e48);
+    color: #fff;
+  }
+`;
