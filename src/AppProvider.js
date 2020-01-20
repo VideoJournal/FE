972ccfbd86184cb46aef7c7e1001ccaf79176d20
Sorin Chis <kish.sorin@yahoo.com>
2020-01-20 17:02:22 +0200

    added Context API

---
+++ src/AppProvider.js
@@ -0,0 +1,41 @@
+import React, { useReducer, createContext, useEffect } from "react";
+
+const SET_CURRENT_VIDEO = "SET_CURRENT_VIDEO";
+
+const initialState = {
+  videosData: [],
+  currentVideo: ""
+};
+
+export const AppContext = createContext();
+
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case SET_CURRENT_VIDEO:
+      return {
+        ...state,
+        currentVideo: payload
+      };
+    default:
+      return state;
+  }
+};
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    //not yes implmemented
+    //fetchVideoData();
+  }, []);
+
+  const setCurentVideo = currentVideoURL => {
+    dispatch({ type: SET_CURRENT_VIDEO, payload: currentVideoURL });
+  };
+
+  const value = { state, setCurentVideo };
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export default AppProvider;
