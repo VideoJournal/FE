ce1d53c0905557c9beeae5eb0d21e1dc43221676
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-31 17:38:54 +0100

    Added image and text

--- src/components/GoogleButton/GoogleButton.js
+++ src/components/GoogleButton/GoogleButton.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default () => <Button></Button>;
+export default () => (
+  <Button>
+    <img
+      src="https://img.icons8.com/color/48/000000/google-logo.png"
+      alt="Google"
+    ></img>
+    <p>Sign in with Google</p>
+  </Button>
+);
 
-const Button = styled.div``;
+const Button = styled.button`
+  background: white;
+`;
