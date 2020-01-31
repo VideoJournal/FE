00d1a5506214794a6572711b683ba5750304c213
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-31 17:23:54 +0100

    The button

---
+++ src/components/GoogleButton/GoogleButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => (
+  <div id="gSignInWrapper">
+    <span class="label">Sign in with:</span>
+    <div id="customBtn" class="customGPlusSignIn">
+      <span class="icon"></span>
+      <span class="buttonText">Google</span>
+    </div>
+  </div>
+);
